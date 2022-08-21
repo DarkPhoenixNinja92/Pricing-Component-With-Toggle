701a916213064f26ef8d64eceb0c63909096ede1
Shaun <scpour@gmail.com>
2022-08-21 12:17:50 -0400

    1.20

--- js/script.js
+++ js/script.js
@@ -1,20 +1,39 @@
 const slider = document.querySelector('.slider');
-const annualMonthly = document.querySelector('.price-monthly-basic');
-const annualLine = document.querySelector('.price-annual-basic-line');
-const annualYearly = document.querySelector('.price-annual-basic');
-const monthlyLine = document.querySelector('.price-monthly-basic-line');
+const priceMonthly = document.querySelectorAll('.price-monthly');
+const priceAnnual = document.querySelectorAll('.price-annual');
+const monthlyLine = document.querySelectorAll('.price-monthly-line');
+const annualLine = document.querySelectorAll('.price-annual-line');
 
 slider.addEventListener('click', function() {
 	if(slider.classList.contains('left')) {
-		annualMonthly.classList.remove('hide');
-		annualYearly.classList.add('hide');
-		annualLine.classList.add('hide');
-		monthlyLine.classList.remove('hide');
+		priceMonthly.forEach(monthlyPrice => {
+			monthlyPrice.classList.remove('hide');
+		});
+		priceAnnual.forEach(annualPrice => {
+			annualPrice.classList.add('hide');
+		})
+		annualLine.forEach(lineAnnual => {
+			lineAnnual.classList.add('hide');
+		})
+		monthlyLine.forEach(lineMonthly => {
+			lineMonthly.classList.remove('hide');
+		})
+		// monthlyLine.classList.remove('hide');
 		slider.classList.add('right');
 		slider.classList.remove('left');
 	} else if(slider.classList.contains('right')) {
-		annualMonthly.classList.add('hide');
-		annualYearly.classList.remove('hide');
+		priceMonthly.forEach(monthlyPrice => {
+			monthlyPrice.classList.add('hide');
+		});
+		priceAnnual.forEach(annualPrice => {
+			annualPrice.classList.remove('hide');
+		})
+		annualLine.forEach(lineAnnual => {
+			lineAnnual.classList.remove('hide');
+		})
+		monthlyLine.forEach(lineMonthly => {
+			lineMonthly.classList.add('hide');
+		})
 		slider.classList.add('left');
 		slider.classList.remove('right');
 	}
